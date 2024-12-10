ef8fc33e8b06ed56df1202b082eadc889c8b603f
Drishya Poudel <drishya.pou@gmail.com>
2024-12-10 16:00:07 +0545

    upodate

--- src/components/body/about/About.js
+++ src/components/body/about/About.js
@@ -2,21 +2,30 @@ import React from "react";
 import "./About.css";
 
 const About = () => {
-    return (
-        <div className="about">
-            <div className="about-top">
-                <div className="about-info">
-                    Hello There 👋, I am
-                    <br />
-                    <span className='info-name'>Drishya Poudel.</span>
-                    <br />I love experimenting with the web.
-                </div>
-                <div className="about-img">
-                  <img src={require('../../../assets/coding.png')} className='picture' />
-                </div>
+   return (
+      <div className="about">
+         <div className="about-top">
+            <div className="about-info">
+               Hello There 👋, I am a
+               {/* <span className="info-name">Drishya Poudel.</span> */}
+               <br />
+               <span style={{ fontWeight: "700", fontSize: "30px" }}>
+                  {" "}
+                  Front-End Develper
+               </span>{" "}
+               and
+               <br />I love experimenting with the web.
             </div>
-        </div>
-    );
+            <div className="about-img">
+               <img
+                  src={require("../../../assets/coding.png")}
+                  className="picture"
+                  alt=""
+               />
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default About;
