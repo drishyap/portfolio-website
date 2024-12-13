8021670c21eaa94ce9683e608f1a9bc9795403f1
Drishya Poudel <drishya.pou@gmail.com>
2024-12-13 15:07:10 +0545

    update code

--- src/components/data/Work.js
+++ src/components/data/Work.js
@@ -1,12 +1,12 @@
 export const WorkData = [
-   {
-      company: "Mero Vision (Part time)",
-      designation: "Front-End (React JS) Developer",
-      dateJoining: "05-10-2024",
-      dateEnd: "Present",
-      companyLogo: require("../../assets/work/nepdroid.png"),
-      work: "Working as a Lead Front-End Web Developer(React JS, NEXT JS)",
-   },
+   // {
+   //    company: "Mero Vision (Part time)",
+   //    designation: "Front-End (React JS) Developer",
+   //    dateJoining: "05-10-2024",
+   //    dateEnd: "Present",
+   //    companyLogo: require("../../assets/work/nepdroid.png"),
+   //    work: "Working as a Lead Front-End Web Developer(React JS, NEXT JS)",
+   // },
    {
       company: "Scodus Innovations",
       designation: "React JS Developer",
